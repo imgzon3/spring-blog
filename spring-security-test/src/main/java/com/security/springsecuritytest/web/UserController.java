fd831fd142bae53e48cd2d82eecc93d4d73f4a62
imgzon3 <imgzon3@naver.com>
2021-07-25 20:41:59 +0900

    GET 요청 로그아웃 추가

--- spring-security-test/src/main/java/com/security/springsecuritytest/web/UserController.java
+++ spring-security-test/src/main/java/com/security/springsecuritytest/web/UserController.java
@@ -3,8 +3,14 @@ package com.security.springsecuritytest.web;
 import com.security.springsecuritytest.service.UserService;
 import com.security.springsecuritytest.web.dto.UserInfoDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @RequiredArgsConstructor
 @Controller
@@ -15,6 +21,13 @@ public class UserController {
     @PostMapping("/user") // signup api
     public String signup(UserInfoDto infoDto) {
         userService.save(infoDto);
+        return "login";
+    }
+
+    @GetMapping("/logout") // logout by GET 요청
+    public String logoutPage(HttpServletRequest request, HttpServletResponse response) {
+        new SecurityContextLogoutHandler().logout(request, response, SecurityContextHolder
+                .getContext().getAuthentication());
         return "login";
     }
 
